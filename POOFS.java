ad3202bc2204eb0ac578af76329c645def884198
Ro3sk1 <61096405+Ro3sk1@users.noreply.github.com>
2024-11-14 15:06:22 +0000

    Commit inicial

---
+++ POOFS.java
@@ -0,0 +1,18 @@
+public class POOFS {
+    public static void main(String[] args) {
+        System.out.println("""
+                __________________________________
+                |           < M E N U >           |
+                | 1 . Criar ou editar cliente     |
+                | 2 . Mostrar lista de clientes   |
+                | 3 . Criar ou editar faturas     |
+                | 4 . Mostrar lista de faturas    |
+                | 5 . Visualizar fatura           |
+                | 6 . Importar faturas            |
+                | 7 . Exportar faturas            |
+                | 8 . Mostrar estatísticas        |
+                | 0 . Terminar programa           |
+                |_________________________________|""");
+    }
+}
+
