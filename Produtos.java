5c12780c3cae0cdea2c3270ebe2b0b57c5fa433f
Ro3sk1 <61096405+Ro3sk1@users.noreply.github.com>
2024-11-14 15:25:24 +0000

    Atualização

---
+++ Produtos.java
@@ -0,0 +1,8 @@
+public class Produtos {
+    protected String codigo;
+    protected String nome;
+    protected String descricao;
+    protected int quantidade;
+    protected double valor_unitario;
+    
+}
